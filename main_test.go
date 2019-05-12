81ec62228b85e80db98d304ecdd322f0017e2b72
mattdamon108 <woonki.moon@gmail.com>
2019-05-13 00:10:08 +0900

    add main_test.go withouth goroutine

--- main_test.go
+++ main_test.go
@@ -1,11 +1,53 @@
 package main
 
 import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 	"testing"
+
+	"github.com/fsnotify/fsnotify"
 )
 
-func TestCommand(t *testing.T) {
-	cmd := []string{"a", "b", "c"}
+func TestExampleCode(t *testing.T) {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		t.Error(err)
+	}
+	defer watcher.Close()
 
-	t.Log(cmd)
+	err = watcher.Add(".")
+	if err != nil {
+		t.Error(err)
+	}
+
+	sigs := make(chan os.Signal)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+MAIN:
+	for {
+		select {
+		case event, ok := <-watcher.Events:
+			if !ok {
+				// break MAIN
+				continue
+			}
+			log.Println("event:", event)
+			if event.Op&fsnotify.Write == fsnotify.Write {
+				log.Println("modified file:", event.Name)
+			}
+
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				// break MAIN
+				continue
+			}
+			log.Println("error:", err)
+
+		case signal := <-sigs:
+			if signal == os.Interrupt {
+				break MAIN
+			}
+		}
+	}
 }
